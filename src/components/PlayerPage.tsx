6a63f4afa926872ea62bcaa2e1ac4ead2be39114
Duke Hill <baltibronx@gmail.com>
2024-12-20 00:24:29 -0500

    Update level percentile message

--- src/components/PlayerPage.tsx
+++ src/components/PlayerPage.tsx
@@ -124,7 +124,7 @@ export default function PlayerPage(props: any) {
                       alignItems="center"
                       style={{ backgroundColor: playerData.MLB_AbbName ? teamColors[playerData.MLB_AbbName][0] : teamColors["FA"][0], paddingLeft: "10px", paddingBottom: "10px", marginBottom: "10px", wordWrap: "break-word"  }}
                     ><img src={Ohtani} height="64px" alt="" style={{ marginLeft: "10px", marginTop: "10px" }} />
-                      <span style={{ marginLeft: "10px", fontSize: "18pt", fontWeight: "bold", marginTop: "10px", color: playerData.MLB_AbbName ? teamColors[playerData.MLB_AbbName][1] : teamColors["FA"][1] }}>AAA Hitting Percentiles</span>
+                      <span style={{ marginLeft: "10px", fontSize: "18pt", fontWeight: "bold", marginTop: "10px", color: playerData.MLB_AbbName ? teamColors[playerData.MLB_AbbName][1] : teamColors["FA"][1] }}>{playerData.level==="AAA"?"AAA":"High A"} Hitting Percentiles</span>
 
                     </Grid>}
 
